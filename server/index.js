eeeb49ee0b4367369efd7f4966b5b5c9d005b078
Ritikj8090 <ritikj713@gmail.com>
2024-03-11 22:32:00 +0530

    Update puppeteer and chrome-aws-lambda dependencies and remove unnecessary code.

--- server/index.js
+++ server/index.js
@@ -1,27 +1,14 @@
 const app = require("express")();
 
-let chrome = {};
-let puppeteer;
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-} else {
-  puppeteer = require("puppeteer");
-}
+chrome = require("chrome-aws-lambda");
+puppeteer = require("puppeteer-core");
 
 app.get("/api", async (req, res) => {
-  let options = {};
-
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
+  options = {
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: true,
+  };
 
   try {
     let browser = await puppeteer.launch(options);
@@ -36,7 +23,7 @@ app.get("/api", async (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Server started 5000");
+  console.log("Server started");
 });
 
 module.exports = app;
